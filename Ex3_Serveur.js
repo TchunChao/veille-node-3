e5e2c5c6889240d1ee9e0c3cf1af2e2916c773a4
Tchun Chao <chaot_xp@live.fr>
2018-02-02 15:42:20 -0500

    Exercice 3 fini

---
+++ Ex3_Serveur.js
@@ -0,0 +1,15 @@
+const http = require("http"); 
+
+http.createServer(function(request, response) 
+{
+	let html = "<table>";
+	const fs = require("fs");
+	response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+	let data = fs.readFileSync('Ex3_provinces.JSON');
+	let obj = JSON.parse(data);
+	for (var p in obj) {
+		html += "<tr><td>" + p + "</td><td>" + obj[p] + "</td></tr>";
+	}
+	response.write(html);
+	response.end();
+}).listen(3000);
